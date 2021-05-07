af703bea9b338d71e787840d3501e5babbd2db12
knskarthik13 <knskarthik13@gmail.com>
2021-05-07 18:35:09 +0530

    application start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { ProductsDetailsComponent } from './products-details/products-details.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    ProductsDetailsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
