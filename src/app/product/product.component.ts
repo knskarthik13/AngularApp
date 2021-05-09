03d4fddc5f381127e82a312867a49952968d31eb
knskarthik13 <knskarthik13@gmail.com>
2021-05-09 19:55:05 +0530

    products page updated

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -9,35 +9,48 @@ export class ProductComponent  {
 
   products:Product[]=[
     {
-    productTitle:"Technology-1",
-    description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
-    productImage:"https://youmatter.world/app/uploads/sites/2/2019/11/tech-planet.jpg"
+    productTitle:"Canon EOS 1500D",
+    description:"Canon EOS 1500D 24.1 Digital SLR Camera (Black) with EF S18-55 is II Lens",
+    productImage:"https://m.media-amazon.com/images/I/914hFeTU2-L._AC_UL480_FMwebp_QL65_.jpg"
     },
     {
-    productTitle:"Technology-2",
-    description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
-    productImage:"https://apicms.thestar.com.my/uploads/images/2020/08/24/831125.jpg"
-    },
-    
-    {
-    productTitle:"Technology-3",
-    description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
-    productImage:"https://itconsultingindubai.files.wordpress.com/2016/03/choosing-a-technology-solution.jpg"
+    productTitle:"Canon EOS 200D ",
+    description:"Canon EOS 200D II 24.1MP Digital SLR Camera + EF-S 18-55mm f4 is STM Lens (Black)",
+    productImage:"https://m.media-amazon.com/images/I/81cEKnH692L._AC_UL480_FMwebp_QL65_.jpg"
     },
     
     {
-    productTitle:"Technology-4",
-    description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
-    productImage:"https://itconsultingindubai.files.wordpress.com/2016/03/choosing-a-technology-solution.jpg"
-    }
+    productTitle:"Canon PowerShot SX540HS",
+    description:"Canon PowerShot SX540HS 20.3MP Digital Camera with 50x Optical Zoom (Black)",
+    productImage:"https://m.media-amazon.com/images/I/71Xp-K4MMBL._AC_UL480_FMwebp_QL65_.jpg"
+    },
+    
+    {
+    productTitle:"Canon M50",
+    description:"Canon M50 Mark II 15-45mm f3.5-6.3 is STM",
+    productImage:"https://m.media-amazon.com/images/I/51bHhi0DMiL._AC_UL480_FMwebp_QL65_.jpg"
+    },
+    {
+      productTitle:"Sony Alpha ILCE",
+      description:"Sony Alpha ILCE 6000L 24.3 MP Mirrorless Digital SLR Camera with 16-50 mm (APS-C Sensor, Fast Auto Focus, Eye AF, Light Weight) - Black",
+      productImage:"https://m.media-amazon.com/images/I/71vLVOCKD+L._AC_UL480_FMwebp_QL65_.jpg"
+      },
+      {
+        productTitle:"Sony Alpha ILCE-7M3K",
+        description:"Sony Alpha ILCE-7M3K Full-Frame 24.2MP Mirrorless Camera with 28-70mm Zoom Lens (4K Full Frame, Real-Time Eye Auto Focus, Tiltable LCD, Low Light Camera) - Black",
+        productImage:"https://m.media-amazon.com/images/I/71CWotTQT4L._AC_UL480_FMwebp_QL65_.jpg"
+        }
+
   ];
 
   productSentByChild=[];
   productCount:number=0;
 
   getProductDetailsFromChild(productTitle){
+    if (!this.productSentByChild.includes(productTitle)){
     this.productSentByChild.push(productTitle);
     this.productCount++;
+    }
   }
 
 
