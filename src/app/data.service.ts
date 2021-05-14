c9d96be6ce220ffd3b29368cc56179e28d4a513f
knskarthik13 <knskarthik13@gmail.com>
2021-05-14 22:53:56 +0530

    services and data updated

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -18,38 +18,38 @@ export class DataService {
   private mobiles:Product[]=[
 
     {
-    productTitle:"Samsung",
-    description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
+    productTitle:"Samsung Galaxy M02s",
+    description:"Samsung Galaxy M02s (Blue,4GB RAM, 64GB Storage) | 5000 mAh | Triple Camera",
     productImage:"https://www.sathya.in/Media/Default/Thumbs/0038/0038136-vivo-s1-pro-black8gb-ram-128gb-storage-250.jpg"
     },
     {
-    productTitle:"Samsung",
-    description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
+    productTitle:"Samsung Galaxy M12",
+    description:"Samsung Galaxy M12 (Blue,4GB RAM, 64GB Storage) 6000 mAh with 8nm Processor",
     productImage:"https://priceintanzania.com/wp-content/uploads/2019/04/Samsung-Galaxy-A70-Price-in-Tanzania.jpg"
     },
     
     {
-    productTitle:"Honor",
-    description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
+    productTitle:"Redmi Note 9 Pro",
+    description:"Redmi Note 9 Pro (Interstellar Black, 4GB RAM, 64GB Storage)- Latest 8nm Snapdragon 720G",
     productImage:"https://5.imimg.com/data5/AW/IX/GLADMIN-53160395/galaxy-s-500x500.png"
     },
     
     {
     productTitle:"Iphone-11",
-    description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
-    productImage:"https://img.tatacliq.com/images/i7/1348Wx2000H/MP000000005562292_1348Wx2000H_202011300234051.jpeg"
+    description:"Iphone-11 6.1-inch (15.5 cm) Liquid Retina HD LCD display with True Tone | Dual 12MP cameras ",
+    productImage:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPnUpuHOI1WPovC3CUemRnhJW4_frjhQy6ow&usqp=CAU"
     },
     
     {
-    productTitle:"OnePlus",
-    description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
+    productTitle:"OnePlus Nord 5G",
+    description:"OnePlus Nord 5G (Blue Marble, 12GB RAM, 256GB Storage) 6.44-inch 90Hz fluid Amoled display ",
     productImage:"https://670354.smushcdn.com/2260778/wp-content/uploads/2021/03/AMZ101-2.jpg"
     },
     
     {
-    productTitle:"Google pixel",
-    description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
-    productImage:"https://www.91-img.com/pictures/105546-v1-google-pixel-xl-mobile-phone-large-1.jpg"
+    productTitle:"Google Pixel 4a",
+    description:"Google Pixel 4a 5G (Just Black, 6GB RAM, 128GB Storage) With HD Duo screen sharing",
+    productImage:"https://m.media-amazon.com/images/I/71C0OH4WfpL._AC_UY327_FMwebp_QL65_.jpg"
     }
     
     ]
--- src/app/televisiondata.service.ts
+++ src/app/televisiondata.service.ts
@@ -10,38 +10,38 @@ export class TelevisiondataService {
   private televisions:Product[]=[
 
     {
-    productTitle:"Samsung",
-    description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
-    productImage:"https://www.sathya.in/Media/Default/Thumbs/0038/0038136-vivo-s1-pro-black8gb-ram-128gb-storage-250.jpg"
+    productTitle:"Mi TV L32M5-AL",
+    description:"Mi 80 cm (32 inches) HD Ready Android Smart LED TV 4A PRO|L32M5-AL (Black)",
+    productImage:"https://m.media-amazon.com/images/I/513lkJuezIL.jpg"
     },
     {
-    productTitle:"Samsung",
-    description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
-    productImage:"https://priceintanzania.com/wp-content/uploads/2019/04/Samsung-Galaxy-A70-Price-in-Tanzania.jpg"
-    },
-    
-    {
-    productTitle:"Honor",
-    description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
-    productImage:"https://5.imimg.com/data5/AW/IX/GLADMIN-53160395/galaxy-s-500x500.png"
+    productTitle:"OnePlus TV 32Y1",
+    description:"OnePlus 80 cm (32 inches) Y Series HD Ready LED Smart Android TV 32Y1 (Black) (2020 Model)",
+    productImage:"https://m.media-amazon.com/images/I/51sIfBeY3sL.jpg"
     },
     
     {
-    productTitle:"Iphone-11",
-    description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
-    productImage:"https://img.tatacliq.com/images/i7/1348Wx2000H/MP000000005562292_1348Wx2000H_202011300234051.jpeg"
+    productTitle:"Samsung TV UA32T4340AKXXL",
+    description:"Samsung 80 cm (32 Inches) Wondertainment Series HD Ready LED Smart TV UA32T4340AKXXL",
+    productImage:"https://m.media-amazon.com/images/I/51OmRL6TZLL.jpg"
     },
     
     {
-    productTitle:"OnePlus",
-    description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
-    productImage:"https://670354.smushcdn.com/2260778/wp-content/uploads/2021/03/AMZ101-2.jpg"
+    productTitle:"LG TV 32LM563BPTC",
+    description:"LG 80 cm (32 inches) HD Ready Smart LED TV 32LM563BPTC (Dark Iron Gray) (2020 Model)",
+    productImage:"https://m.media-amazon.com/images/I/51STyIbNXuL.jpg"
     },
     
     {
-    productTitle:"Google pixel",
-    description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
-    productImage:"https://www.91-img.com/pictures/105546-v1-google-pixel-xl-mobile-phone-large-1.jpg"
+    productTitle:"AmazonBasics TV AB32E10SS",
+    description:"AmazonBasics 81cm (32 inches) Fire TV Edition HD Ready Smart LED TV AB32E10SS (Black)",
+    productImage:"https://m.media-amazon.com/images/I/811j4G5MJML._AC_UL480_FMwebp_QL65_.jpg"
+    },
+    
+    {
+    productTitle:"Sony Bravia TV KDL-43W6603",
+    description:"Sony Bravia 108 cm (43 inches) Full HD Smart LED TV KDL-43W6603 (Black) (2020 Model)",
+    productImage:"https://m.media-amazon.com/images/I/81lpllAGcBL._AC_UL480_FMwebp_QL65_.jpg"
     }
     
     ]
